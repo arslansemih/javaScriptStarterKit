30cc88c32cb8f25bc0785484d6a1102f7a8fce1e
Semih Arslan <semih.arslan@trendyol.com>
2021-10-31 12:12:19 +0300

    31 Ekim ders sonu

---
+++ src/app.js
@@ -0,0 +1,10 @@
+
+
+let konutKredisi = ["Konut Kredisi", "Emlak Kredisi","Kamu Konut Kredisi"]
+console.log("<ul>")
+
+for (let i = 0; i < konutKredisi.length; i++) {
+    console.log("<li>"+konutKredisi[i]+"</li>")}
+
+console.log("</ul>")   
+
